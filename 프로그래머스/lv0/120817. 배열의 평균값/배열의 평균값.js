398765646a8a2cc19136661943b1aa6493b61da6
uniqueeest <123538138+uniqueeest@users.noreply.github.com>
2023-03-09 23:25:04 +0900

    [level 0] Title: 배열의 평균값, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+    var answer = 0;
+    for (let i =0; i < numbers.length; i++) {
+        answer += parseInt(numbers[i]);
+    }
+    
+    return answer / numbers.length;
+}
