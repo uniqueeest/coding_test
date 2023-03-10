23a416ab1c88f1f888254dbd2b9f828d95785a44
uniqueeest <123538138+uniqueeest@users.noreply.github.com>
2023-03-10 21:21:51 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+    var answer = 0;
+    numbers.sort((a, b) => a - b);
+    
+    answer = numbers[numbers.length-1] * numbers[numbers.length-2];
+    
+    return answer;
+}
