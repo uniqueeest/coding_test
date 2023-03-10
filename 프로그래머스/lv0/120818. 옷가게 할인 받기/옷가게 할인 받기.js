1cd80306086ef0e2371d1648462e011aad97214d
uniqueeest <123538138+uniqueeest@users.noreply.github.com>
2023-03-10 21:12:09 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,11 +1,11 @@
 function solution(price) {
     var answer = 0;
     if (price >= 500000) {
-        answer = parseInt(Math.floor(price * 0.8));
+        answer = parseInt(price * 0.8);
     } else if (price >= 300000) {
-        answer = parseInt(Math.floor(price * 0.9));
+        answer = parseInt(price * 0.9);
     } else if (price >= 100000) {
-        answer = parseInt(Math.floor(price * 0.95));
+        answer = parseInt(price * 0.95);
     } else {
         answer = price;
     }
