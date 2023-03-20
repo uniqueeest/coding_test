86d348c9bf2372edee6bf5472cfa9be63eaa385c
uniqueeest <123538138+uniqueeest@users.noreply.github.com>
2023-03-20 22:45:43 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    var answer = 45;
+    let sum = 0;
+    for (let i=0; i<numbers.length; i++) {
+        sum += numbers[i];
+    }
+        
+    
+    return answer - sum;
+}
