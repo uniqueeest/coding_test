cfaf1014b56d575b690e149fc991a566b7b4e654
uniqueeest <123538138+uniqueeest@users.noreply.github.com>
2023-03-20 23:36:45 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    var answer = s.split("").sort(function(a, b) {
+  if(a < b) return 1;
+  if(a > b) return -1;
+  if(a === b) return 0;
+});
+    return answer.join("");
+}
