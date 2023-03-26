2bc63aebd433a18f6b01f3d17c54705cb68382e3
uniqueeest <123538138+uniqueeest@users.noreply.github.com>
2023-03-26 22:24:57 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 16.11 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,15 @@
+function solution(arr)
+{
+    var answer = [];
+    answer.push(arr[0])
+    for (let i=1; i<arr.length; i++) {
+        if (arr[i] !== arr[i-1]) {
+            answer.push(arr[i]);
+        }
+    }
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+    
+    return answer;
+}
