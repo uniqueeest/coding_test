a8595f59fe98c0df14ea86548b7fe8ccc32678ac
uniqueeest <123538138+uniqueeest@users.noreply.github.com>
2023-04-09 09:48:29 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -0,0 +1,30 @@
+function solution(id_list, report, k) {
+    // 중복 제거
+    let reports = [...new Set(report)].map(a => {
+        return a.split(" ");
+    });
+    
+    // userList
+    const userList = id_list.reduce((list, id) => {
+        list[id] = [0, []];
+        return list;
+    }, {});
+    
+    
+    for (let id of reports) {
+        const [reportID, ID] = id;
+        userList[reportID][1].push(ID); //신고한 유저
+        userList[ID][0]++               //신고된 횟수
+        
+    }
+    
+    //K이상 신고횟수
+    const ban = id_list.filter((a) => userList[a][0] >= k);
+    const newMap = id_list.map((a) => {
+        return userList[a][1].filter((a) => {
+            return ban.includes(a);
+        }).length;
+    })
+    
+    return newMap;
+}
