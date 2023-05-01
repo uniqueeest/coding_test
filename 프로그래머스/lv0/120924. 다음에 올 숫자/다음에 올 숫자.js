8f74096c08f6fc1c034b6fd7794a17c0360c489b
최윤재 <123538138+uniqueeest@users.noreply.github.com>
2023-05-01 22:37:27 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,8 @@
+function solution(common) {
+    let answer = 0;
+    if (common[1] - common[0] === common[2] - common[1]) {
+        return common[common.length - 1] + common[2] - common[1];
+    } else {
+        return common[common.length - 1] * (common[1] / common[0]);
+    }
+}
