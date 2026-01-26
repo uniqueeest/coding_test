e7ec86c0ae8037cb5c37abdd043261f2334163c1
Yoonjae Choi <dbswo9795@gmail.com>
2026-01-26 19:32:23 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 160 ms, Memory: 9668 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.js
@@ -0,0 +1,24 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+const solution = (input) => {
+  const N = Number(input[0]);
+  const arr = input[1].split(' ').map(Number);
+
+  const dp = Array.from({ length: N + 1 }, () => 1);
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j <= i; j++) {
+      if (arr[i] < arr[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  return Math.max(...dp);
+};
+
+console.log(solution(input));
